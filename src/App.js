16a637e6fea5cead9752f250ed22d3686e4a0af2
plum-shiga <ume0224@gmail.com>
2020-02-29 22:52:11 +0900

    [add] basepage

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Card from '@material-ui/core/Grid';
+
+const App = () => {
+  return (
+    <div>
+      <Card>
+        <p>basepage</p>
+      </Card>
+    </div>
+  );
+}
+
+export default App
