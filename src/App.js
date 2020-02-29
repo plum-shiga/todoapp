d32906d3a3ea3e5bfc9768579e9c45592c4bd6c5
plum-shiga <ume0224@gmail.com>
2020-02-29 23:03:27 +0900

    [add] todo form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '@material-ui/core/Grid';
 
+import Form from './form.js';
+
 const App = () => {
+  const [todos, setTodo] = useState([
+  ]);
   return (
     <div>
+      <Form todos={todos} setTodo={setTodo} />
       <Card>
         <p>basepage</p>
       </Card>
