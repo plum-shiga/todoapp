03daadaba4af17610eae1cd28bf30be3842cac4e
plum-shiga <ume0224@gmail.com>
2020-02-29 23:04:00 +0900

    [add] todo item component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Card from '@material-ui/core/Grid';
 
 import Form from './form.js';
+import Item from './item.js';
 
 const App = () => {
   const [todos, setTodo] = useState([
@@ -10,7 +11,9 @@ const App = () => {
     <div>
       <Form todos={todos} setTodo={setTodo} />
       <Card>
-        <p>basepage</p>
+        {todos.map((todo) => (
+          <Item key={todo.key} todo={todo} />
+        ))}
       </Card>
     </div>
   );
